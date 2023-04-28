3dcdf0c1117782a4f1a9e6c46c8675404f6d9774
amar deep <adeep2400@gmail.com>
2023-04-28 14:53:02 +0530

    created footer with responsive

--- webique-frontend/src/Pages/Contactus.jsx
+++ webique-frontend/src/Pages/Contactus.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import "./ContactUss.css";
+import { Link } from "react-router-dom";
 
 const initialState={
     name:"",
@@ -9,25 +11,73 @@ const initialState={
 
 const ContactUs=()=>{
     const [query,setQuery]=useState(initialState);
+    const [message,setMessage]=useState("");
+    const reff=useRef();
 
     const handleSubmit=(e)=>{
         e.preventDefault();
-        if(query.email==="" || query.number===""){
-            alert("Please enter your email or phone number");
+        if(query.email==="" && query.number===""){
+            setMessage("Please enter your email or phone number");
+            reff.current.style.display="block";
+            reff.current.id="MessageIn";
+            setQuery(initialState);
+            let timeOut=setTimeout(()=>{
+                reff.current.id="MessageOut";
+                clearTimeout(timeOut);
+            },2000);
+            let timeOutTwo=setTimeout(()=>{
+                reff.current.style.display="none";
+                reff.current.id="message";
+                clearTimeout(timeOutTwo);
+            },2460);
         } else {
-            console.log(query);
+            setMessage("Thank you for contacting us, we will reach you soon...");
+            reff.current.style.display="block";
+            reff.current.id="MessageIn";
+            setQuery(initialState);
+            let timeOut=setTimeout(()=>{
+                reff.current.id="MessageOut";
+                clearTimeout(timeOut);
+            },2000);
+            let timeOutTwo=setTimeout(()=>{
+                reff.current.style.display="none";
+                reff.current.id="message";
+                clearTimeout(timeOutTwo);
+            },2460);
         }
     };
 
-    return <div>
-        <h1>ContactUs</h1>
-        <form onSubmit={(e)=>handleSubmit(e)}>
-            <input type="text" placeholder="Enter your name" value={query.name} onChange={(e)=>setQuery({...query,name:e.target.value})}/>
-            <input type="text" placeholder="Enter your e-mail" value={query.email} onChange={(e)=>setQuery({...query,email:e.target.value})}/>
-            <input type="text" placeholder="Enter your phone number" value={query.mobile} onChange={(e)=>setQuery({...query,mobile:e.target.value})}/>
-            <textarea placeholder="Enter your message" value={query.message} onChange={(e)=>setQuery({...query,message:e.target.value})} required></textarea>
-            <input type="submit" name="" value="SUBMIT" />
-        </form>
+    return <div id="Contact">
+        <h1 style={{fontFamily:"serif"}}>GET IN TOUCH</h1>
+        <div>
+            <div id="ContactAddress">
+                <div id="Email">
+                    <h1>Email Id :</h1>
+                    <a href="mailto:amar1998deep@gmail.com">amar1998deep@gmail.com</a>
+                </div>
+                <hr/>
+                <div id="Phone">
+                    <h1>Phone Number :</h1>
+                    <a href="tel:+916388898362">+91-6388898362</a>
+                </div>
+                <hr/>
+                <div id="Add">
+                    <h1>Address :</h1>
+                    <p>Robertsganj, Sonbhadra</p>
+                    <p>Uttar Pradesh, 231216</p>
+                </div>
+            </div>
+            <form onSubmit={(e)=>handleSubmit(e)} id="Form">
+                <input type="text" placeholder="Enter your name" value={query.name} onChange={(e)=>setQuery({...query,name:e.target.value})}/>
+                <input type="email" placeholder="Enter your e-mail" value={query.email} onChange={(e)=>setQuery({...query,email:e.target.value})}/>
+                <input type="tel" placeholder="Enter your phone number" value={query.mobile} onChange={(e)=>setQuery({...query,mobile:e.target.value})}/>
+                <textarea placeholder="Enter your message" value={query.message} onChange={(e)=>setQuery({...query,message:e.target.value})} required></textarea>
+                <button type="submit">SUBMIT</button>
+            </form>
+        </div>
+        <div id="message" ref={reff} style={{display:"none"}}>
+            {message}
+        </div>
     </div>
 }
 
